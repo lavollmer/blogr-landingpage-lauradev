9df8b910b274e936fced9471edd9a6f4d8cfb675
Laura <laura.e.vollmer@gmail.com>
2025-01-02 14:28:33 -0600

    adding style guide colors recommended

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,15 @@
 export default {
   content: [],
   theme: {
-    extend: {},
+    extend: {
+      "light-red": "356, 100%, 66%",
+      "very-light-red": "355, 100%, 74%",
+      "very-dark-blue": "208, 49%, 24%",
+      white: "0, 0%, 100%",
+      "grayish-blue": "240, 2%, 79%",
+      "very-dark-bluish": "207, 13%, 34%",
+      "very-dark-black-blue": "240, 10%, 16%",
+    },
   },
   plugins: [],
-}
-
+};
