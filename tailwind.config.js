187834ef8abc01522cba2882506c0c369b99925d
Laura <laura.e.vollmer@gmail.com>
2025-01-02 14:33:59 -0600

    updated the config file for colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,13 +3,15 @@ export default {
   content: [],
   theme: {
     extend: {
-      "light-red": "356, 100%, 66%",
-      "very-light-red": "355, 100%, 74%",
-      "very-dark-blue": "208, 49%, 24%",
-      white: "0, 0%, 100%",
-      "grayish-blue": "240, 2%, 79%",
-      "very-dark-bluish": "207, 13%, 34%",
-      "very-dark-black-blue": "240, 10%, 16%",
+      colors:{
+        "light-red": "hsl(356, 100%, 66%)",
+        "very-light-red": "hsl(355, 100%, 74%)",
+        "very-dark-blue": "hsl(208, 49%, 24%)",
+        white: "hsl(0, 0%, 100%)",
+        "grayish-blue": "hsl(240, 2%, 79%)",
+        "very-dark-bluish": "hsl(207, 13%, 34%)",
+        "very-dark-black-blue": "hsl(240, 10%, 16%)",
+      },
     },
   },
   plugins: [],
