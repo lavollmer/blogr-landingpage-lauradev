15b541bd9f6a483a6ed2daf1d0b1d2142f908ee1
Laura <laura.e.vollmer@gmail.com>
2025-01-03 16:23:31 -0600

    updating the Future component for the body

--- src/components/Future.tsx
+++ src/components/Future.tsx
@@ -1,5 +1,21 @@
 const Future = () => {
-  return <div>Future</div>;
+  return (
+    <div className="flex flex-col text-very-dark-grayish-blue">
+      <div>
+        <h1>Designed for the Future</h1>
+      </div>
+      <div className="flex flex-row">
+        <div>
+          <h2>Introducing an extensibble editor</h2>
+          <p>Bingo</p>
+        </div>
+        <div>
+          <h2>Robust content management</h2>
+          <p>Part 2</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Future;
