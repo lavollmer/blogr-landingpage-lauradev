b2c36462c28a68fa614d7269c2d0f33726d6e9ee
Laura <laura.e.vollmer@gmail.com>
2025-01-03 15:54:40 -0600

    updating navigation bar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -3,17 +3,22 @@ import Logo from "../assets/logo.svg";
 
 const NavBar = () => {
   return (
-    <div className="flex flex-row">
+    <div className="flex flex-row bg-very-light-red">
+      <div className="flex flex-row space-x-4 w-full p-4">
         <div>
-            <img src={Logo} alt="Logo" />
+          <img src={Logo} alt="Logo" />
         </div>
-        <div>
-            <button>Home</button>
-            <button>About</button>
-            <button>Contact</button>
+        <div className="flex flex-row space-x-4">
+          <button>Product</button>
+          <button>Company</button>
+          <button>Connect</button>
         </div>
+      </div>
+      <div>
+        <button>Login</button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default NavBar
+export default NavBar;
