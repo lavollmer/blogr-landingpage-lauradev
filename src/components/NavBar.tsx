2b8b3f777e4f010bce6c571f8dedfa9ee46462d8
Laura <laura.e.vollmer@gmail.com>
2025-01-03 16:12:03 -0600

    styling the signup button and login button

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -4,7 +4,7 @@ import DownArrow from "../assets/icon-arrow-light.svg";
 
 const NavBar = () => {
   return (
-    <div className="flex flex-row text-white bg-very-light-red text-sm p-6">
+    <div className="flex flex-row text-white text-ubuntu bg-very-light-red text-sm p-6">
       <div className="flex flex-row space-x-4 w-full p-4">
         <div>
           <img src={Logo} alt="Logo" />
@@ -14,11 +14,11 @@ const NavBar = () => {
             <h1>Product</h1>
             <img src={DownArrow} alt="Down Arrow" />
           </button>
-          <button className="flex flex-row items-center">
+          <button className="flex flex-row items-center space-x-2">
             <h1>Company</h1>
             <img src={DownArrow} alt="Down Arrow" />
           </button>
-          <button className="flex flex-row  items-center">
+          <button className="flex flex-row  items-center space-x-2">
             <h1>Connect</h1>
             <img src={DownArrow} alt="Down Arrow" />
           </button>
@@ -26,9 +26,11 @@ const NavBar = () => {
       </div>
       <div className="flex flex-row justify-end space-x-6 w-full bg-very-light-red">
         <h1 className="flex flex-row justify-center items-center">Login</h1>
-        <button className="flex flex-row justify-center items-center rounded-lg bg-white text-very-light-red text-bold">
-          Sign Up
-        </button>
+        <div className="flex flex-row justify-center items-center bg-very-dark-red rounded-full">
+          <button className="bg-white text-very-light-red font-bold py-2 px-4 rounded-full">
+            Sign Up
+          </button>
+        </div>
       </div>
     </div>
   );
