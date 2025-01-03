5a8ccc2d44ca33d9ae58958e289cdbab6e5cabab
Laura <laura.e.vollmer@gmail.com>
2025-01-03 16:18:58 -0600

    finalizing general layout of page

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,9 +4,9 @@ const Header = () => {
   return (
     <div className="bg-very-light-red text-white text-center">
       <NavBar />
-      <div className="flex flex-col justify-center items-center p-10">
+      <div className="flex flex-col justify-center items-center p-10 space-y-4">
         <div>
-          <h1 className="flex flex-row">A modern publishing platform</h1>
+          <h1 className="flex flex-row text-4xl">A modern publishing platform</h1>
         </div>
         <div>
           <h2>Grow your audience and build your online brand</h2>
@@ -18,7 +18,7 @@ const Header = () => {
             </button>
           </div>
           <div className="flex flex-row justify-center items-center rounded-full">
-            <button className="bg-very-light-red text-white font-bold py-2 px-4 rounded-full">
+            <button className="bg-very-light-red text-white font-bold py-2 px-4 border border-white rounded-full">
               Learn More
             </button>
           </div>
