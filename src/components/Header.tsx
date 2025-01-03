23f5d63ad5036f6d8a5d94b37a76fdd575fcee87
Laura <laura.e.vollmer@gmail.com>
2025-01-03 16:15:44 -0600

    creating the header for the blog landing page

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,23 @@
-import NavBar from "./NavBar"
+import NavBar from "./NavBar";
 
 const Header = () => {
   return (
-    <div>
-        <NavBar />
+    <div className="bg-very-light-red text-white text-center">
+      <NavBar />
+      <div className="flex flex-col justify-center items-center p-10">
+        <div>
+          <h1 className="flex flex-row">A modern publishing platform</h1>
+        </div>
+        <div>
+          <h2>Grow your audience and build your online brand</h2>
+        </div>
+        <div>
+          <button>Start for Free</button>
+          <button>Learn More</button>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
