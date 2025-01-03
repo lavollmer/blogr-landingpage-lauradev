f60a7496dfa1c99a2cf8b978a8d4b155f4aa6a91
Laura <laura.e.vollmer@gmail.com>
2025-01-03 16:01:11 -0600

    aligning items for product, company and connect

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,17 +1,27 @@
 import "../index.css";
 import Logo from "../assets/logo.svg";
+import DownArrow from "../assets/icon-arrow-light.svg";
 
 const NavBar = () => {
   return (
-    <div className="flex flex-row bg-very-light-red">
+    <div className="flex flex-row text-white bg-very-light-red">
       <div className="flex flex-row space-x-4 w-full p-4">
         <div>
           <img src={Logo} alt="Logo" />
         </div>
-        <div className="flex flex-row space-x-4">
-          <button>Product</button>
-          <button>Company</button>
-          <button>Connect</button>
+        <div className="flex flex-row space-x-4 text-sm">
+          <button className="flex flex-row items-center">
+            Product
+            <img src={DownArrow} alt="Down Arrow" />
+          </button>
+          <button className="flex flex-row items-center">
+            Company
+            <img src={DownArrow} alt="Down Arrow" />
+          </button>
+          <button className="flex flex-row  items-center">
+            Connect
+            <img src={DownArrow} alt="Down Arrow" />
+          </button>
         </div>
       </div>
       <div>
