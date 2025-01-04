912b559524697e1b8fc094c44580a0cfeb068e3e
Laura <laura.e.vollmer@gmail.com>
2025-01-04 09:35:28 -0600

    styling with padding of art section complete

--- src/components/Art.tsx
+++ src/components/Art.tsx
@@ -2,16 +2,16 @@ import Phones from "../assets/illustration-phones.svg";
 
 const Art = () => {
   return (
-    <div className="flex flex-row">
+    <div className="flex flex-row text-white bg-very-dark-blue p-10">
       <div>
         <img src={Phones} alt="Phones" />
       </div>
-      <div className="flex flex-col justify-center">
+      <div className="flex flex-col justify-center p-10">
         <div>
-          <h1>State of the Art Infrastructure</h1>
+          <h1 className="font-bold text-2xl">State of the Art Infrastructure</h1>
         </div>
         <div>
-          <p>
+          <p className="text-sm text-left">
             With reliability and speed in mind, worldwide data centers provide
             the backbone for ultra-fast connectivity. This ensures your site
             will load instantly no matter where your readers are, keeping your
