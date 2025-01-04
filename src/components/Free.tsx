88be79740edb68af8da7f8e872f53defaa843d33
Laura <laura.e.vollmer@gmail.com>
2025-01-04 09:39:33 -0600

    general template of free component completed

--- src/components/Free.tsx
+++ src/components/Free.tsx
@@ -1,11 +1,15 @@
+import Laptop from "../assets/illustration-laptop-desktop.svg";
+
 const Free = () => {
   return (
-    <div>
-      <div></div>
+    <div className="flex flex-row">
       <div>
+        <img src={Laptop} alt="laptop" />
+      </div>
+      <div className="flex flex-col space-y-4 p-10">
         <div>
-          <h1>Free,open,simple</h1>
-          <p>
+          <h1 className="font-bold">Free, open, simple</h1>
+          <p className="text-sm text-left">
             Blogr is a free and open source application backed by a large
             community of helpful developers. It supports features such as code
             syntax highlighting, RSS feeds, social media integration,
@@ -15,8 +19,8 @@ const Free = () => {
           </p>
         </div>
         <div>
-          <h1>Powerful tooling</h1>
-          <p>
+          <h1 className="font-bold">Powerful tooling</h1>
+          <p className="text-sm text-left">
             Batteries included. We built a simple and straightforward CLI tool
             that makes customization and deployment a breeze, but capable of
             producing even the most complicated sites.
