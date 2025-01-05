839e52b5fc9b905a8f9fbdf3fdd793ecc672a65c
Laura <laura.e.vollmer@gmail.com>
2025-01-05 10:16:51 -0600

    formatting the dropdown menu component

--- src/components/Dropdown.tsx
+++ src/components/Dropdown.tsx
@@ -1,11 +1,11 @@
 const Dropdown = () => {
   return (
     <div>
-      <div class="relative inline-block text-left">
+      <div className="relative inline-block text-left">
         <div>
           <button
             type="button"
-            class="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
+            className="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
             id="menu-button"
             aria-expanded="true"
             aria-haspopup="true"
@@ -13,49 +13,33 @@ const Dropdown = () => {
             Product
           </button>
         </div>
-        {/* 
-  <!--
-    Dropdown menu, show/hide based on menu state.
-
-    Entering: "transition ease-out duration-100"
-      From: "transform opacity-0 scale-95"
-      To: "transform opacity-100 scale-100"
-    Leaving: "transition ease-in duration-75"
-      From: "transform opacity-100 scale-100"
-      To: "transform opacity-0 scale-95"
-  --> */}
         <div
-          class="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black/5 focus:outline-none"
+          className="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black/5 focus:outline-none"
           role="menu"
           aria-orientation="vertical"
           aria-labelledby="menu-button"
-          tabindex="-1"
         >
-          <div class="py-1" role="none">
-            {/* <!-- Active: "bg-gray-100 text-gray-900 outline-none", Not Active: "text-gray-700" --> */}
+          <div className="py-1" role="none">
             <a
               href="#"
-              class="block px-4 py-2 text-sm text-gray-700"
+              className="block px-4 py-2 text-sm text-gray-700"
               role="menuitem"
-              tabindex="-1"
               id="menu-item-0"
             >
               Account settings
             </a>
             <a
               href="#"
-              class="block px-4 py-2 text-sm text-gray-700"
+              className="block px-4 py-2 text-sm text-gray-700"
               role="menuitem"
-              tabindex="-1"
               id="menu-item-1"
             >
               Support
             </a>
             <a
               href="#"
-              class="block px-4 py-2 text-sm text-gray-700"
+              className="block px-4 py-2 text-sm text-gray-700"
               role="menuitem"
-              tabindex="-1"
               id="menu-item-2"
             >
               License
@@ -63,9 +47,8 @@ const Dropdown = () => {
             <form method="POST" action="#" role="none">
               <button
                 type="submit"
-                class="block w-full px-4 py-2 text-left text-sm text-gray-700"
+                className="block w-full px-4 py-2 text-left text-sm text-gray-700"
                 role="menuitem"
-                tabindex="-1"
                 id="menu-item-3"
               >
                 Sign out
