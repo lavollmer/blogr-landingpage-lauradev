be4294aabb95acf804927bb9c1d733c5808051ff
Laura <laura.e.vollmer@gmail.com>
2025-01-06 10:58:19 -0600

    styling the header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,23 +2,25 @@ import NavBar from "./NavBar";
 
 const Header = () => {
   return (
-    <div className="bg-gradient-to-r from-very-light-red to-gradient-light-red text-white text-center">
+    <div className="bg-gradient-to-r from-very-light-red to-gradient-light-red text-white text-center pl-20 pr-20 pt-10">
       <NavBar />
-      <div className="flex flex-col justify-center items-center p-10 space-y-4">
+      <div className="flex flex-col justify-center items-center p-10 space-y-4 pb-40">
         <div>
-          <h1 className="flex flex-row text-4xl">A modern publishing platform</h1>
+          <h1 className="flex flex-row text-4xl">
+            A modern publishing platform
+          </h1>
         </div>
         <div>
           <h2>Grow your audience and build your online brand</h2>
         </div>
         <div className="flex flex-row justify-center text-center space-x-4">
           <div className="flex flex-row justify-center items-center rounded-full">
-            <button className="bg-white text-very-light-red font-bold py-2 px-4 rounded-full">
+            <button className="bg-very-light-red text-white font-bold py-2 px-4 border border-white rounded-full">
               Start for Free
             </button>
           </div>
           <div className="flex flex-row justify-center items-center rounded-full">
-            <button className="bg-very-light-red text-white font-bold py-2 px-4 border border-white rounded-full">
+            <button className="bg-white text-very-light-red font-bold py-2 px-4 rounded-full">
               Learn More
             </button>
           </div>
