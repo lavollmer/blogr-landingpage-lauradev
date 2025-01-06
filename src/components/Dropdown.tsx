210842e746fa125b818690aba3f691fdf7df7d62
Laura <laura.e.vollmer@gmail.com>
2025-01-06 10:46:46 -0600

    debugging the spread of the application across the whole picture

--- src/components/Dropdown.tsx
+++ src/components/Dropdown.tsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
+import DownArrow from "../assets/icon-arrow-light.svg";
 
+//defined the DropdownProps interrface with a string dropdownName
 interface DropdownProps {
   dropdownName: string;
 }
@@ -7,6 +9,7 @@ interface DropdownProps {
 const Dropdown: React.FC<DropdownProps> = ({ dropdownName }) => {
   const [isOpen, setIsOpen] = useState(false);
 
+  //change the toggleDropdown with useState and setISOpen
   const toggleDropdown = () => {
     setIsOpen(!isOpen);
   };
@@ -14,29 +17,17 @@ const Dropdown: React.FC<DropdownProps> = ({ dropdownName }) => {
   return (
     <div>
       <div className="relative inline-block text-left">
-        <div>
+        <div className="flex flex-row">
           <button
             type="button"
-            className="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-very-light-red px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
+            className="inline-flex w-full justify-center items-center gap-x-1.5 rounded-md bg-very-light-red px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
             id="menu-button"
             aria-expanded={isOpen}
             aria-haspopup="true"
             onClick={toggleDropdown}
           >
             {dropdownName}
-            <svg
-              className="-mr-1 size-5 text-gray-400"
-              viewBox="0 0 20 20"
-              fill="currentColor"
-              aria-hidden="true"
-              data-slot="icon"
-            >
-              <path
-                fill-rule="evenodd"
-                d="M5.22 8.22a.75.75 0 0 1 1.06 0L10 11.94l3.72-3.72a.75.75 0 1 1 1.06 1.06l-4.25 4.25a.75.75 0 0 1-1.06 0L5.22 9.28a.75.75 0 0 1 0-1.06Z"
-                clip-rule="evenodd"
-              />
-            </svg>
+            <img src={DownArrow} alt="Down Arrow" />
           </button>
         </div>
         {isOpen && (
