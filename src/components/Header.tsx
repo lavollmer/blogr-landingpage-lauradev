5fd2c7dd8eba49803740a9aeaedb5a0266094ac7
Laura <laura.e.vollmer@gmail.com>
2025-01-07 11:26:00 -0600

    working on the header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -10,21 +10,21 @@ const Header = () => {
         <NavBar />
         <div className="flex flex-col justify-center items-center p-10 space-y-4 pb-40">
           <div>
-            <h1 className="flex flex-row text-4xl">
+            <h1 className="flex flex-row text-6xl">
               A modern publishing platform
             </h1>
           </div>
           <div>
-            <h2>Grow your audience and build your online brand</h2>
+            <h2 className="text-xl">Grow your audience and build your online brand</h2>
           </div>
-          <div className="flex flex-row justify-center text-center space-x-4">
+          <div className="flex flex-row justify-center text-center space-x-4 pt-10">
             <div className="flex flex-row justify-center items-center rounded-full">
-              <button className="bg-very-light-red text-white font-bold py-2 px-4 border border-white rounded-full">
+              <button className="bg-very-light-red text-white font-bold py-2 px-4 rounded-full">
                 Start for Free
               </button>
             </div>
             <div className="flex flex-row justify-center items-center rounded-full">
-              <button className="bg-white text-very-light-red font-bold py-2 px-4 rounded-full">
+              <button className="bg-white bg-opacity-90 text-very-light-red font-bold py-2 px-4 rounded-full">
                 Learn More
               </button>
             </div>
