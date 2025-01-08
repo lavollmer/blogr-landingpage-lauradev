11c7b8838776d359a7fc38834118b446baf70d3d
Laura <laura.e.vollmer@gmail.com>
2025-01-08 12:49:33 -0600

    creating the sidenavigationbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -5,9 +5,7 @@ import Dropdown from "./Dropdown";
 const NavBar = () => {
   return (
     <div className="flex flex-row text-white bg-transparent text-md p-6 font-overpass">
-      <div
-        className="flex flex-row space-x-10 w-full p-4"
-      >
+      <div className="flex flex-row space-x-10 w-full p-4">
         <div>
           <img src={Logo} alt="Logo" />
         </div>
@@ -24,9 +22,11 @@ const NavBar = () => {
         </div>
       </div>
       <div className="flex flex-row justify-end space-x-6 w-full bg-transparent">
-        <button className="flex flex-row justify-center items-center">
-          Login
-        </button>
+        <div className="flex flex-row justify-center items-center ">
+          <button className="flex flex-row justify-center items-center">
+            Login
+          </button>
+        </div>
         <div className="flex flex-row justify-center items-center bg-very-dark-red rounded-full">
           <button className="bg-transparent text-white font-bold py-2 px-4 rounded-full hover:bg-white hover:bg-opacity-20">
             Sign Up
--- src/components/SideNavBar.tsx
+++ src/components/SideNavBar.tsx
@@ -1,7 +1,38 @@
+import IconArrowDark from "../assets/icon-arrow-dark.svg";
+
 const SideNavBar = () => {
   return (
-    <div>SideNavBar</div>
-  )
-}
+    <div>
+      <div>
+        <div className="flex flex-col space-y-4 font-300">
+          <button className="flex flex-row justify-center items-center space-x-2">
+            <h1>Products</h1>
+            <img src={IconArrowDark} alt="Arrow" />
+          </button>
+          <button className="flex flex-row justify-center items-center space-x-2">
+            <h1>Company</h1>
+            <img src={IconArrowDark} alt="Arrow" />
+          </button>
+          <button className="flex flex-row justify-center items-center space-x-2">
+            <h1>Connect</h1>
+            <img src={IconArrowDark} alt="Arrow" />
+          </button>
+        </div>
+      </div>
+      <div>
+        <div className="flex flex-row justify-center items-center ">
+          <button className="flex flex-row justify-center items-center">
+            Login
+          </button>
+        </div>
+        <div className="flex flex-row justify-center items-center bg-very-dark-red rounded-full">
+          <button className="bg-transparent text-white font-bold py-2 px-4 rounded-full hover:bg-white hover:bg-opacity-20">
+            Sign Up
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default SideNavBar
+export default SideNavBar;
