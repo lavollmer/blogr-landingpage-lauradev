3d211913b4c626c02f6cbe41810338ac14fe2eaa
Laura <laura.e.vollmer@gmail.com>
2025-01-08 13:08:40 -0600

    formatting navigation bars for size of screen

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,14 @@ import "./App.css";
 import Header from "./components/Header";
 import Body from "./components/Body";
 import Footer from "./components/Footer";
-import SideNavBar from "./components/SideNavBar";
 
 function App() {
   return (
     <>
       <div className="flex flex-col h-screen">
-        <SideNavBar />
-        <div>
-          <Header />
-          <Body />
-          <Footer />
-        </div>
+        <Header />
+        <Body />
+        <Footer />
       </div>
     </>
   );
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,5 @@
 import NavBar from "./NavBar";
+import SideNavBar from "./SideNavBar";
 import "../index.css";
 
 const Header = () => {
@@ -7,7 +8,13 @@ const Header = () => {
       <div className="absolute inset-0 bg-gradient-to-r from-very-light-red to-gradient-light-red"></div>
       <div className="absolute inset-0 bg-header-texture"></div>
       <div className="relative z-10">
-        <NavBar />
+      {/* navigation components displayed depending on size of screen */}
+        <div className="hidden md:block">
+          <NavBar />
+        </div>
+        <div className="md:hidden">
+          <SideNavBar />
+        </div>
         <div className="flex flex-col justify-center items-center pt-20 p-10 space-y-4 pb-40">
           <div>
             <h1 className="flex flex-row text-6xl font-normal">
@@ -15,7 +22,9 @@ const Header = () => {
             </h1>
           </div>
           <div>
-            <h2 className="text-xl">Grow your audience and build your online brand</h2>
+            <h2 className="text-xl">
+              Grow your audience and build your online brand
+            </h2>
           </div>
           <div className="flex flex-row justify-center items-center space-x-4 pt-10 font-semibold">
             <div className="flex flex-row justify-center items-center rounded-full">
